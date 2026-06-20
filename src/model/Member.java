709ffb6e687341a07a37cac2d20652c3c265a29f
Saron <yitbareksaron60@gmail.com>
2026-06-20 12:55:55 -0700

    Added A member class that extends the Person class and includes a membership type attribute, along with a method to display member information.

--- src/model/Member.java
+++ src/model/Member.java
@@ -1,5 +1,34 @@
 package model;
 
-public class Member {
-    
+public class Member extends Person {
+
+    private String membershipType;
+
+    public Member() {
+        super();
+    }
+
+    public Member(int id,
+                  String name,
+                  int age,
+                  String phone,
+                  String membershipType) {
+
+        super(id, name, age, phone);
+        this.membershipType = membershipType;
+    }
+
+    public String getMembershipType() {
+        return membershipType;
+    }
+
+    public void setMembershipType(String membershipType) {
+        this.membershipType = membershipType;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Membership Type: " + membershipType);
+    }
 }
