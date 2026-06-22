6ad1e454ebe84891fcb1c3cf4697a2f1245a2a17
kj633 <kidusjony9@gmail.com>
2026-06-22 15:00:54 -0700

    new added file

--- src/file/BackupManager.java
+++ src/file/BackupManager.java
@@ -1,5 +1,38 @@
 package file;
 
+import model.Member;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 public class BackupManager {
-    
+
+    public static void backupMembers(
+            ArrayList<Member> members) {
+
+        try {
+
+            ObjectOutputStream out =
+                    new ObjectOutputStream(
+                            new FileOutputStream(
+                                    "members.dat"
+                            )
+                    );
+
+            out.writeObject(members);
+
+            out.close();
+
+            System.out.println(
+                    "Backup completed."
+            );
+
+        } catch(Exception e) {
+
+            System.out.println(
+                    e.getMessage()
+            );
+        }
+    }
 }
