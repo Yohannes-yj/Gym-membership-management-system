6f57eb8150a2ad09c33fc98f055c7d1e4c795ab1
kj633 <kidusjony9@gmail.com>
2026-06-22 13:04:46 -0700

    Extend QuarterlyPlan from MembershipPlan

--- src/model/QuarterlyPlan.java
+++ src/model/QuarterlyPlan.java
@@ -1,5 +1,13 @@
 package model;
 
-public class QuarterlyPlan {
-    
+public class QuarterlyPlan extends MembershipPlan {
+
+    public QuarterlyPlan() {
+        super("Quarterly Plan");
+    }
+
+    @Override
+    public double calculateFee() {
+        return 1300;
+    }
 }
